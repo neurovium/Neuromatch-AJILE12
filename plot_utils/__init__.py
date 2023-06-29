5b6d523d1a4ab088f98e18fc168a61e074c6ce61
Nima Dehghani <nima.dehghani@mit.edu>
2023-06-29 01:01:29 -0400

    nit fix

--- plot_utils/__init__.py
+++ plot_utils/__init__.py
@@ -1,5 +1,5 @@
-from clabels import *
-from elecs import identify_elecs, plot_ecog_descript, plot_ecog_elec, plot_ecog_electrodes_mni_from_nwb_file, _plot_electrodes
-from pow import plot_ecog_pow, _ecog_pow_group, _ecog_pow_single
-from traj import plot_wrist_trajs, _get_wrist_trajs, plot_dlc_recon_errs
-from data import load_data_characteristics
+from plot_utils.clabels import clabel_table_create, prune_clabels, plot_clabels
+from plot_utils.elecs import identify_elecs, plot_ecog_descript, plot_ecog_elec, plot_ecog_electrodes_mni_from_nwb_file, _plot_electrodes
+from plot_utils.pow import plot_ecog_pow, _ecog_pow_group, _ecog_pow_single
+from plot_utils.traj import plot_wrist_trajs, _get_wrist_trajs, plot_dlc_recon_errs
+from plot_utils.data import load_data_characteristics
