3349e6d8b7a8dedcf669f4c62678a28015710d1b
Nima Dehghani <nima.dehghani@mit.edu>
2023-06-28 22:52:00 -0400

    setup update

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+
+with open("LICENSE.txt") as f:
+    license = f.read()
+    
+with open("requirements.txt", "r") as f:
+    required = f.read().splitlines()
+
 setup(
     name="Neuromatch-AJILE12",
     version="1.0.0",
@@ -8,5 +17,5 @@ setup(
     author_email="nima.dehghani@mit.edu",
     url="https://github.com/neurovium/Neuromatch-AJILE12",
     packages=["plot_utils", "spec_utils"],
-    install_requires=requirements,
+    install_requires=required,
 )
